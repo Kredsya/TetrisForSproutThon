509500ecbd15d829e094528670b68c8db5769cef
Yoonho Hann <jk16400@naver.com>
2021-06-29 00:18:36 +0900

    1칸 블럭 쌓기, 점수 카운트, 하드드롭 구현

--- TetrisForSproutThon.cpp
+++ TetrisForSproutThon.cpp
@@ -1,8 +1,9 @@
 ﻿//블럭있는 보드 좌표 (18,1)부터 (36,20)까지 x +2, y +1 증가시켜 이용    
 //mapData는 보드 아래부터 인덱스 시작
 
-//@todo: 점수 카운트, 블럭 벡터, 블럭 벡터 이동 제한, 블럭쌓기 구현 (회전은 쌓기 구현 이후)
+//@todo: 점수 카운트, 블럭 벡터, 블럭 벡터 이동 제한, 블럭쌓기 구현 (회전은 쌓기 구현 이후)     -> clear?
 //@todo: 다음에 나올 블럭 표시 추가 + 홀드 구현 추가?        <-좌표 이용 새로운 함수 생성
+//@todo: 게임오버 처리
 
 #include "head.h"
 
@@ -17,7 +18,7 @@ void draw_block(BlockData, int, int);
 void game_over();
 void exit_game();
 void player_move(int);
-void draw_board(int);
+void draw_board(bool);
 
 typedef struct BlockData {
     int type;
@@ -26,6 +27,8 @@ typedef struct BlockData {
 
 int x = 26, y = 1;
 int key;
+int cnt = 0;        //쌓기 판정용
+bool fall = false;
 int score = 0;               // 점수
 vector<pair<vector<bool>, int>> mapData;
 vector<bool> emptyLine(10, false);	//vector constructor 중 하나 : 변수명(개수, 초기화할 데이터);
@@ -53,8 +56,13 @@ int main() {
                 ch = _getch();
                 player_move(ch);               //방향키는 224->n으로 입력되어 space는 따로 구현
             }
-            //else if (ch == SPACE)                     //@todo: space로 hard drop 구현
-
+            else if (ch == SPACE) {                 //@todo: space로 hard drop 구현
+                while (true) {
+                    player_move(DOWN);
+                    if (x == 26 && y == 1)
+                        break;
+                }
+            }
                                                 //@todo: 홀드 구현시 shift 추가 
 
         }
@@ -63,7 +71,8 @@ int main() {
             if (mapData[i].second == 10) {
                 mapData.erase(mapData.begin() + i);
                 mapData.push_back(make_pair(emptyLine, 0));
-                draw_board(i);                   //다시 그리기
+                draw_board(0);                   //다시 그리기
+                score += 20;                //줄 지워서 점수 획득
             }
         }
 
@@ -142,19 +151,28 @@ void fillNextBlock() {
     }
 }
 
-void draw_board(int n) {            //@todo: 보드를 다시 그릴 때 이동중인 블럭이 지워지지 않는지 확인
-
-    while (mapData[n].second != 0){            //빈 줄이 나올 때까지 올라가면서 다시 출력
-        gotoxy(18, 21 - n);              //mapData는 아래부터 시작, 좌표는 위부터 시작으로 계산
-        for (int i = 0; i < FR_SIZE_WIDTH; i++) {
-            if (mapData[n].first[i])
-                cout << BLOCK_MARK;
-            else
-                cout << NONE_MARK;
+void draw_board(bool tf) {            //@todo: 보드를 다시 그릴 때 이동중인 블럭이 지워지지 않는지 확인
+        //true: 쌓아서 그리기만 false: 전체 다시 그리기
+    if (tf) {
+        for (int i = 1; i <= FR_SIZE_HEIGHT; i++) {
+            for (int j = 0; j < FR_SIZE_WIDTH; j++) {
+                if (mapData[20 - i].first[j]) {
+                    gotoxy(18 + 2 * j, i);
+                    cout << BLOCK_MARK;
+                }
+            }
         }
-        n++;
-        if (n > 20)
-            break;
+    }
+    else {
+        for (int i = 1; i <= FR_SIZE_HEIGHT; i++) {
+            gotoxy(18, i);
+            for (int j = 0; j < FR_SIZE_WIDTH; j++) {
+                if (mapData[20 - i].first[j])
+                    cout << BLOCK_MARK;
+                else
+                    cout << NONE_MARK;
+            }
+        }
     }
 }
 
@@ -162,7 +180,7 @@ void player_move(int ch) {       //*다 쌓이면 game_over로 이동
     int a = x;              //원래 xy좌표 저장
     int b = y;
 
-    cout << "\b\b";
+    cout << "\b\b";             
     cout << NONE_MARK;
 
     //@todo: up으로 회전 추가
@@ -172,6 +190,7 @@ void player_move(int ch) {       //*다 쌓이면 game_over로 이동
         break;
     case DOWN:
         y++;
+        fall = true;
         if (y > 20)
             y = 20;
         break;
@@ -187,12 +206,30 @@ void player_move(int ch) {       //*다 쌓이면 game_over로 이동
         break;
     }
 
-    if (mapData[21 - y].first[x - 18])          //@todo: mapData 비교로 못 움직이게 추가
+    if (mapData[20 - y].first[(x - 18) / 2])          //@todo: mapData 비교로 못 움직이게 추가
         x = a, y = b;
 
+    if (x == a && y == b && fall) { //움직이지 못했을 경우
+        cnt++;
+        fall = false;
+    }
+
+    if (cnt == 3) {
+        cnt = 0;
+        fall = false;
+        mapData[20 - y].first[(x - 18) / 2] = 1;
+        mapData[20 - y].second += 1;
+        draw_board(1);
+        x = 26, y = 1;
+        score += 10;                //블럭 쌓아서 점수 획득
+    }
+
     gotoxy(x, y);
+
+    //draw_block()으로 변경
     cout << BLOCK_MARK;        //처음 플레이어 위치 표시용 (플레이어 움직임 구현 시연용)
-                                
+    
+
                                //@todo: 옮길 수 있는 것인지에 대한 체크가 선행되어야 함
                                 //@todo: draw_block으로 블럭 그리기를 할 예정
 }
